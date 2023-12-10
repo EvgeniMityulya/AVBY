0fb65ef39e1f09b09d87b06736b7729a71e987d8
iseedeadpeople <evgenni2004@gmail.com>
2023-12-10 15:33:27 +0300

    edit style navigation, add carImagesCollectionView, rounded images, colors, images

---
+++ AVBY/Common/Extensions/UITabBarController + Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UITabBarController + Extensions.swift
+//  AVBY
+//
+//  Created by Евгений Митюля on 12/9/23.
+//
+
+import Foundation
---
+++ AVBY/View Controllers/SearchViewController/CatalogViewController/Custom/CarImagesCollectionView/CarImagesCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CarImagesCollectionViewCell.swift
+//  AVBY
+//
+//  Created by Евгений Митюля on 12/9/23.
+//
+
+import UIKit
+
+class CarImagesCollectionViewCell: UICollectionViewCell {
+    
+}
